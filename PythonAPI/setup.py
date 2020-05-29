0b093440fe9e0438b068ccc44398b54d41fe1007
Ben Talberg <ben.talberg@gmail.com>
2020-05-29 17:40:41 -0500

    fix requirements install issues for pypi release

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -1,9 +1,18 @@
-from setuptools import setup, Extension
-import numpy as np
+from setuptools import dist, setup, Extension
 
 # To compile and install locally run "python setup.py build_ext --inplace"
 # To install library to Python site-packages run "python setup.py build_ext install"
 
+install_requires = [
+    'setuptools>=18.0',
+    'cython>=0.27.3',
+    'matplotlib>=2.1.0',
+    'numpy>=1.18.4'
+]
+
+dist.Distribution().fetch_build_eggs(install_requires)
+
+import numpy as np
 ext_modules = [
     Extension(
         'pycocotools._mask',
@@ -14,14 +23,13 @@ ext_modules = [
 ]
 
 setup(
-    name='pycocotools',
+    name='wf-pycocotools',
     packages=['pycocotools'],
-    package_dir = {'pycocotools': 'pycocotools'},
-    install_requires=[
-        'setuptools>=18.0',
-        'cython>=0.27.3',
-        'matplotlib>=2.1.0'
-    ],
-    version='2.0',
-    ext_modules= ext_modules
+    package_dir={'pycocotools': 'pycocotools'},
+    install_requires=install_requires,
+    version='2.0.1',
+    url='https://github.com/WildflowerSchools/cocoapi',
+    author='Piotr Dollar, Tsung-Yi Lin, Benjamin Jaffe-Talberg',
+    author_email='ben.talberg@wildflowerschools.org',
+    ext_modules=ext_modules
 )
