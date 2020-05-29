f4b961e66e68f3df2392d38f601f6d9cb6351e8c
Ben Talberg <ben.talberg@gmail.com>
2020-05-29 18:54:22 -0500

    add common dir as symbolic link to resolve build time issue not finding required c files

--- PythonAPI/setup.py
+++ PythonAPI/setup.py
@@ -1,4 +1,4 @@
-from setuptools import dist, setup, Extension
+from setuptools import dist, find_packages, setup, Extension
 
 # To compile and install locally run "python setup.py build_ext --inplace"
 # To install library to Python site-packages run "python setup.py build_ext install"
@@ -16,18 +16,19 @@ import numpy as np
 ext_modules = [
     Extension(
         'pycocotools._mask',
-        sources=['../common/maskApi.c', 'pycocotools/_mask.pyx'],
-        include_dirs = [np.get_include(), '../common'],
+        sources=['./common/maskApi.c', 'pycocotools/_mask.pyx'],
+        include_dirs=[np.get_include(), './common'],
         extra_compile_args=['-Wno-cpp', '-Wno-unused-function', '-std=c99'],
     )
 ]
 
 setup(
     name='wf-pycocotools',
-    packages=['pycocotools'],
-    package_dir={'pycocotools': 'pycocotools'},
+    packages=find_packages(),
+    include_package_data=True,
     install_requires=install_requires,
-    version='2.0.1',
+    description="Forked version of the cocodataset's pycocotools modified to install from pypi without manual intervention",
+    version='2.0.1.1',
     url='https://github.com/WildflowerSchools/cocoapi',
     author='Piotr Dollar, Tsung-Yi Lin, Benjamin Jaffe-Talberg',
     author_email='ben.talberg@wildflowerschools.org',
